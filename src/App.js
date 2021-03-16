6426664d30c652ee779271ceeb33ce6bc2d6d8be
Stefania Popa <stefania_florina10@yahoo.com>
2021-03-16 18:35:03 +0200

    did table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { PersonTable } from './PersonTable';
+
+let teams = 
+    [
+        {
+            "id": "a123",
+            "firstName": "Stefania",
+            "lastName": "Popa",
+            "gitHub": "stefaniapopa"
+        }, 
+        {
+            "id":"b564",
+            "firstName":"Florin",
+            "lastName":"Popa",
+            "gitHub":"Florin2210"
+         },
+        {
+            "id":"c958",
+            "firstName":"Iustin",
+            "lastName":"Popa",
+            "gitHub":"justinpopa20"
+        }
+    ];
 
 function App() {
   return (
     <div>
       <h1>Teams Networking</h1>
       <div>Search</div>
-      <p>table</p>
+      <PersonTable teams={teams} />
     </div>
   );
 }
---
+++ src/teams.js
@@ -0,0 +1,20 @@
+export const teams = [
+    {
+        id: "a123",
+        firstName: "Stefania",
+        lastName: "Popa",
+        gitHub: "stefaniapopa"
+    }, 
+    {
+        id:"b564",
+        firstName:"Florin",
+        lastName:"Popa",
+        gitHub:"Florin2210"
+     },
+    {
+        id:"c958",
+        firstName:"Iustin",
+        lastName:"Popa",
+        gitHub:"justinpopa20"
+    }
+];
