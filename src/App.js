dcb4712f9078436276b36c1e58531b5876ad2e54
Stefania Popa <stefania_florina10@yahoo.com>
2021-03-22 19:34:56 +0200

    change to class

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PersonTable } from './PersonTable';
+import SearchBox from './SearchBox';
+import { teams } from './teams';
+import 'font-awesome/css/font-awesome.min.css';
 
-let teams = 
-    [
-        {
-            "id": "a123",
-            "firstName": "Stefania",
-            "lastName": "Popa",
-            "gitHub": "stefaniapopa"
-        }, 
-        {
-            "id":"b564",
-            "firstName":"Florin",
-            "lastName":"Popa",
-            "gitHub":"Florin2210"
-         },
-        {
-            "id":"c958",
-            "firstName":"Iustin",
-            "lastName":"Popa",
-            "gitHub":"justinpopa20"
-        }
-    ];
 
-function App() {
-  return (
-    <div>
-      <h1>Teams Networking</h1>
-      <div>Search</div>
-      <PersonTable teams={teams} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      teams: teams,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render(){
+    const filterPerson = this.state.teams.filter(team =>{
+      return team.firstName.toLowerCase().includes(this.state.searchfield.toLowerCase()) ||
+      team.lastName.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    return (
+      <div className='tc'>
+        <h1>Teams Networking</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <PersonTable teams={filterPerson} />
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
