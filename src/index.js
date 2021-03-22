f125f12b5a579beca64dcac1ad0b3808749ae983
Stefania Popa <stefania_florina10@yahoo.com>
2021-03-22 19:39:33 +0200

    change some code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './App.css'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
+</React.StrictMode>,
   document.getElementById('root')
 );
 
